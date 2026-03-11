5ccd7b8dc2613d2a88376abdef8d834d0378d9c9
Chibuike Anukam <ganukam2@uwo.ca>
2026-03-11 03:24:17 -0400

    Changed parameters for receiver to match SPARKMAX specifications

--- CAN_BUS_DEMO_WEBOTS_Receiver/CAN_BUS_DEMO_WEBOTS_Receiver.ino
+++ CAN_BUS_DEMO_WEBOTS_Receiver/CAN_BUS_DEMO_WEBOTS_Receiver.ino
@@ -1,24 +1,23 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
-#define CS_PIN 5
+#define CS_PIN  5
 #define INT_PIN 4
 
 MCP_CAN CAN(CS_PIN);
 
 void setup() {
   Serial.begin(115200);
-
   SPI.begin(18, 19, 23, CS_PIN);
 
-  while (CAN_OK != CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ)) {
+  Serial.print("Hi");
+  while (CAN_OK != CAN.begin(MCP_EXT, CAN_1000KBPS, MCP_8MHZ)) {
     Serial.println("CAN init failed, retrying...");
     delay(500);
   }
 
   CAN.setMode(MCP_NORMAL);
   pinMode(INT_PIN, INPUT);
-
   Serial.println("CAN init ok");
 }
 
@@ -32,10 +31,9 @@ void loop() {
 
     Serial.print("ID: 0x");
     Serial.print(rxId, HEX);
-    Serial.print(" Data: ");
-
+    Serial.print("  Data: ");
     for (int i = 0; i < len; i++) {
-      Serial.print(rxBuf[i]);
+      Serial.print(rxBuf[i], HEX);
       Serial.print(" ");
     }
     Serial.println();
